5745b01aed38037947afa036c18f7a9422934458
Mikkel Borring Jakobsen <mikkeljakobsen@hotmail.com>
2020-04-11 23:10:15 +0200

    update

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import numpy as np
 from PIL import Image
 
 def DepthNorm(x, maxDepth):
-    return np.nan_to_num(maxDepth / x)
+    return maxDepth / x
 
 def predict(model, images, minDepth=10, maxDepth=1000, batch_size=2):
     # Support multiple RGBs, one RGB image, even grayscale 
