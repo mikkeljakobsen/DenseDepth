a8b3378d3cf2069bfa0f7742737f3be34f43ae61
Mikkel Borring Jakobsen <mikkeljakobsen@hotmail.com>
2020-04-13 19:29:11 +0200

    update

--- evaluate.py
+++ evaluate.py
@@ -2,6 +2,7 @@ import os
 import glob
 import time
 import argparse
+from utils import load_void_test_data
 
 # Kerasa / TensorFlow
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '5'
@@ -26,18 +27,19 @@ model = load_model(args.model, custom_objects=custom_objects, compile=False)
 # Load test data
 print('Loading test data...', end='')
 import numpy as np
-from data import extract_zip
-data = extract_zip('nyu_test.zip')
-from io import BytesIO
-rgb = np.load(BytesIO(data['eigen_test_rgb.npy']))
-depth = np.load(BytesIO(data['eigen_test_depth.npy']))
-crop = np.load(BytesIO(data['eigen_test_crop.npy']))
+#from data import extract_zip
+#data = extract_zip('nyu_test.zip')
+#from io import BytesIO
+#rgb = np.load(BytesIO(data['eigen_test_rgb.npy']))
+#depth = np.load(BytesIO(data['eigen_test_depth.npy']))
+#crop = np.load(BytesIO(data['eigen_test_crop.npy']))
+test_set = load_void_test_data()
 print('Test data loaded.\n')
 
 start = time.time()
 print('Testing...')
-
-e = evaluate(model, rgb, depth, crop, batch_size=6)
+e = evaluate(model, test_set['rgb'], test_set['depth'], test_set['crop'], batch_size=6, verbose=True)
+#e = evaluate(model, rgb, depth, crop, batch_size=6)
 
 print("{:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}".format('a1', 'a2', 'a3', 'rel', 'rms', 'log_10'))
 print("{:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}".format(e[0],e[1],e[2],e[3],e[4],e[5]))
