a6a3aeb3e88dde9f40ae24ed0cc0a9d3f3b030a3
Mikkel Borring Jakobsen <mikkeljakobsen@hotmail.com>
2020-04-17 08:23:35 +0200

    update

--- utils.py
+++ utils.py
@@ -122,9 +122,8 @@ def load_void_test_data(void_data_path='/home/mikkel/data/void_release', use_spa
 
     interp_depths = []
     if use_sparse_depth:
-        void_test_interp_depth = list(line.strip() for line in open('/home/mikkel/data/void_sparse/void_150/void_test_interp_depth_150.txt'))
-        for interp_depth_path in void_test_interp_depth:
-            img = np.asarray(np.asarray(Image.open( interp_depth_path ))/256.0)
+        for interp_depth_path in void_test_depth:
+            img = np.asarray(np.asarray(Image.open( os.path.join("/home/mikkel/data/void_sparse/", interp_depth_path).replace('ground_truth', 'interp_depth') ))/256.0)
             interp_depths.append(img)
         inds = np.arange(len(interp_depths)).tolist()
         interp_depths = np.array([interp_depths[i] for i in inds])
