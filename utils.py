2b48e6dab4b23be0d09019dc6749e691be3d6c52
Mikkel Borring Jakobsen <mikkeljakobsen@hotmail.com>
2020-04-20 21:35:40 +0200

    evaluate rgb+sparse-depth model

--- utils.py
+++ utils.py
@@ -7,10 +7,13 @@ def DepthNorm(x, maxDepth):
 
 def predict(model, images, minDepth=10, maxDepth=1000, batch_size=2):
     # Support multiple RGBs, one RGB image, even grayscale 
-    #if len(images.shape) < 3: images = np.stack((images,images,images), axis=2)
-    #if len(images.shape) < 4: images = images.reshape((1, images.shape[0], images.shape[1], images.shape[2]))
+    if isinstance(images, list):
+        if len(images[0].shape) < 4: images = [images[0].reshape((1, images[0].shape[0], images[0].shape[1], images[0].shape[2])), images[1].reshape((1, images[1].shape[0], images[1].shape[1]))]
+    else:
+        if len(images.shape) < 3: images = np.stack((images,images,images), axis=2)
+        if len(images.shape) < 4: images = images.reshape((1, images.shape[0], images.shape[1], images.shape[2]))
     # Compute predictions
-    predictions = model.predict(images.tolist(), batch_size=batch_size)
+    predictions = model.predict(images, batch_size=batch_size)
     # Put in expected range
     return np.clip(DepthNorm(predictions, maxDepth=maxDepth), minDepth, maxDepth) / maxDepth
 
