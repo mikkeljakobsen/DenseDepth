9d2d0c5666000ea4d2152877332df9d85112b01b
Mikkel Borring Jakobsen <mikkeljakobsen@hotmail.com>
2020-04-21 21:05:39 +0200

    update

--- utils.py
+++ utils.py
@@ -261,7 +261,7 @@ def compute_errors(gt, pred, min_depth=0.1, max_depth=10.0):
     mae = np.mean(np.abs(gt - pred))
     i_mae = np.mean(np.abs(1.0/gt - 1.0/pred))
     i_rmse = np.sqrt(np.mean((1.0/gt - 1.0/pred)**2))
-    rmse_log = (np.log(gt) - np.log(pr)) ** 2
+    rmse_log = (np.log(gt) - np.log(pred)) ** 2
     rmse_log = np.sqrt(rmse_log.mean())
 
     return a1, a2, a3, abs_rel, rmse, log_10, scinv, mae, i_mae, i_rmse, rmse_log
