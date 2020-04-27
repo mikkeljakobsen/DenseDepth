ab3ac055df1bb7a513f307fe92161d3009f1776d
Mikkel Borring Jakobsen <mikkeljakobsen@hotmail.com>
2020-04-27 12:13:02 +0200

    Train without depth normalization

--- utils.py
+++ utils.py
@@ -307,10 +307,10 @@ def evaluate(model, rgb, depth, crop, batch_size=6, verbose=False, use_median_sc
 
         # Compute results
         true_y = depth[(i)*bs:(i+1)*bs,:,:]
-        pred_y = scale_up(2, predict(model, x/255, minDepth=settings.MIN_DEPTH*settings.DEPTH_SCALE, maxDepth=settings.MAX_DEPTH*settings.DEPTH_SCALE, batch_size=bs)[:,:,:,0]) * settings.MAX_DEPTH
+        pred_y = scale_up(2, predict(model, x/255, minDepth=settings.MIN_DEPTH*settings.DEPTH_SCALE, maxDepth=settings.MAX_DEPTH*settings.DEPTH_SCALE, batch_size=bs)[:,:,:,0])# * settings.MAX_DEPTH
         
         # Test time augmentation: mirror image estimate
-        pred_y_flip = scale_up(2, predict(model, x[...,::-1,:]/255, minDepth=settings.MIN_DEPTH*settings.DEPTH_SCALE, maxDepth=settings.MAX_DEPTH*settings.DEPTH_SCALE, batch_size=bs)[:,:,:,0]) * settings.MAX_DEPTH
+        pred_y_flip = scale_up(2, predict(model, x[...,::-1,:]/255, minDepth=settings.MIN_DEPTH*settings.DEPTH_SCALE, maxDepth=settings.MAX_DEPTH*settings.DEPTH_SCALE, batch_size=bs)[:,:,:,0])# * settings.MAX_DEPTH
 
         # Crop based on Eigen et al. crop
         true_y = true_y[:,crop[0]:crop[1]+1, crop[2]:crop[3]+1]
