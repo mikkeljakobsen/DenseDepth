7838be4a3bf22eeca16f2c659cfbe4ba0699a875
Mikkel Borring Jakobsen <mikkeljakobsen@hotmail.com>
2020-05-01 11:34:01 +0200

    Two-branch different encoders

--- model.py
+++ model.py
@@ -116,7 +116,8 @@ def create_two_branch_model(existing='', is_twohundred=False, is_halffeatures=Tr
             base_model = applications.DenseNet169(input_shape=(None, None, 3), include_top=False, weights='imagenet', input_tensor=input_rgb)
             #base_model_sz = applications.DenseNet169(input_shape=(None, None, 3), include_top=False, weights='imagenet', input_tensor=concatenate([input_sparse, input_sparse, input_sparse], axis=-1))
             base_model_sz = applications.DenseNet121(input_shape=(None, None, channels-3), include_top=False, weights=None, input_tensor=input_sparse)
-        for layer in base_model.layers:
+            pretrained = applications.DenseNet121(input_shape=(None, None, 3), include_top=False, weights='imagenet')
+        for layer in pretrained.layers:
             print(layer.name)
             if layer.get_weights() != []:  # Skip input, pooling and no weights layers
                 target_layer = base_model_sz.get_layer(name=layer.name)
