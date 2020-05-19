6cb89cc5768dd3a829dc4887f3a7dc4e2a2b5e24
mikkeljakobsen <mikkeljakobsen@hotmail.com>
2020-05-19 20:32:41 +0200

    added eval script

--- evaluate_voiced_void.sh
+++ evaluate_voiced_void.sh
@@ -3,6 +3,20 @@
 export CUDA_VISIBLE_DEVICES=0
 
 python evaluate.py \
---model /home/mikkel/final_models/1589616487-n12672-e20-bs4-lr0.0001-nyu-half-features/model.h5 \
+--model /home/mikkel/final_models/nyu/1589616487-n12672-e20-bs4-lr0.0001-nyu-half-features/model.h5 \
 --dataset nyu \
 --use-cpu
+
+#python evaluate.py \
+#--model /home/mikkel/final_models//model.h5 \
+#--dataset nyu \
+#--channels 3 \
+#--use-cpu \
+#--dont-interpolate \
+#--use-void-1500 \
+#--use-median-scaling \
+#--use-sparse-depth-scaling \
+#--use-scaling-array \
+#--path /home/mikkel/data/pico_sommerhus \
+#--gt-divider 1000.0 \
+#--save
