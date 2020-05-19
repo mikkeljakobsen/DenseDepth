42ed37346e68e269dd0abe33a10db7f0463404b2
mikkeljakobsen <mikkeljakobsen@hotmail.com>
2020-05-19 21:00:18 +0200

    added eval script

--- evaluate_voiced_void.sh
+++ evaluate_voiced_void.sh
@@ -9,16 +9,12 @@ export CUDA_VISIBLE_DEVICES=0
 
 python evaluate.py \
 --dataset nyu \
-#--channels 3 \
 --use-cpu \
-#--dont-interpolate \
 --model /home/mikkel/final_models/nyu/1589474496-n12672-e20-bs4-lr0.0001-nyu-all-features/model.h5
 
 python evaluate.py \
 --dataset nyu \
-#--channels 3 \
 --use-cpu \
-#--dont-interpolate \
 --model /home/mikkel/final_models/nyu/1587572185-n6336-e20-bs8-lr0.0001-nyu-resnet50/model.h5
 
 #VOID RGB
@@ -26,28 +22,24 @@ python evaluate.py \
 --dataset void \
 --channels 3 \
 --use-cpu \
-#--dont-interpolate \
 --model /home/mikkel/final_models/void_rgb/1589053397-n12104-e20-bs4-lr0.0001-void-nyu/model.h5
 
 python evaluate.py \
 --dataset void \
 --channels 3 \
 --use-cpu \
-#--dont-interpolate \
 --model /home/mikkel/final_models/void_rgb/1586925390-n12104-e5-bs4-lr0.0001-nyu-void/model.h5
 
 python evaluate.py \
 --dataset void \
 --channels 3 \
 --use-cpu \
-#--dont-interpolate \
 --model /home/mikkel/final_models/void_rgb/densedepth_void_e20-bs4-lr0.001/model.h5
 
 python evaluate.py \
 --dataset void \
 --channels 3 \
 --use-cpu \
-#--dont-interpolate \
 --model /home/mikkel/final_models/void_rgb/1589360045-n12104-e20-bs4-lr0.0001-void-only-all-features/model.h5
 
 #VOID RGBD 150
@@ -69,21 +61,18 @@ python evaluate.py \
 --dataset void \
 --channels 4 \
 --use-cpu \
-#--dont-interpolate \
 --model /home/mikkel/final_models/void_rgbd-150/1588955269-n24207-e10-bs2-lr0.0001-void-1500-late-fusion-imagenet-3+1-iD/model.h5
 
 python evaluate.py \
 --dataset void \
 --channels 4 \
 --use-cpu \
-#--dont-interpolate \
 --model /home/mikkel/final_models/void_rgbd-150/1588876366-n12104-e20-bs4-lr0.0001-void-1500-early-fusion-imagenet-3+1-iD/model.h5
 
 python evaluate.py \
 --dataset void \
 --channels 4 \
 --use-cpu \
-#--dont-interpolate \
 --model /home/mikkel/final_models/void_rgbd-150/1588839176-n12104-e9-bs4-lr0.0001-void-early-fusion-imagenet-3+1-iD-without-depthnorm/model.h5
 
 python evaluate.py \
@@ -97,35 +86,30 @@ python evaluate.py \
 --dataset void \
 --channels 4 \
 --use-cpu \
-#--dont-interpolate \
 --model /home/mikkel/final_models/void_rgbd-150/1588355073-n24207-e29-bs2-lr0.0001-void-two-branch-two-encoders-imagenet-3+1-iD-with-depthnorm/model.h5
 
 python evaluate.py \
 --dataset void \
 --channels 4 \
 --use-cpu \
-#--dont-interpolate \
 --model /home/mikkel/final_models/void_rgbd-150/1588180820-n24207-e6-bs2-lr0.0001-void-two-branch-imagenet-iD-with-depthnorm/model.h5
 
 python evaluate.py \
 --dataset void \
 --channels 4 \
 --use-cpu \
-#--dont-interpolate \
 --model /home/mikkel/final_models/void_rgbd-150/1588085278-n12104-e9-bs4-lr0.0001-void-4channel-imagenet-iD-with-depthnorm-early-fusion/model.h5
 
 python evaluate.py \
 --dataset void \
 --channels 5 \
 --use-cpu \
-#--dont-interpolate \
 --model /home/mikkel/final_models/void_rgbd-150/1587845588-n12104-e20-bs4-lr0.0001-void-5channel-without-depthnorm/model.h5
 
 python evaluate.py \
 --dataset void \
 --channels 5 \
 --use-cpu \
-#--dont-interpolate \
 --model /home/mikkel/final_models/void_rgbd-150/1587726473-n12104-e20-bs4-lr0.0001-void-5channel-3rd-run/model.h5
 
 
