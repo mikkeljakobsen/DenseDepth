f13934401c08c7df5df2109dce3dc9d423e2156a
mikkeljakobsen <mikkeljakobsen@hotmail.com>
2020-05-20 10:43:37 +0200

    make qualitative examples

--- utils.py
+++ utils.py
@@ -394,7 +394,7 @@ def evaluate(model, rgb, depth, crop, batch_size=6, verbose=False, use_median_sc
             if save_pred:
                 import matplotlib.pyplot as plt
                 from skimage.transform import resize
-                path = "/home/mikkel/samples/" + model_name + "/pred_viz_all/" + str((i+1)*(j+1))+".jpg"
+                path = "/home/mikkel/samples/" + model_name + "/pred_viz_all/" + str((i+1)*(j+1))+".png"
                 z = np.uint32(prediction.copy()*256.0)
                 z = Image.fromarray(z, mode='I')
                 save_img(z, path.replace('pred_viz_all','pred_raw_depth'))
