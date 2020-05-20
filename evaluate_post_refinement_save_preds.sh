cf2b3c9085a12118ae8bbf0dc11f83d3613c2ff0
mikkeljakobsen <mikkeljakobsen@hotmail.com>
2020-05-20 11:59:53 +0200

    make qualitative examples

---
+++ evaluate_post_refinement_save_preds.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=0
+#VOID RGB
+python evaluate.py \
+--dataset void \
+--channels 3 \
+--use-cpu \
+--save \
+--model-name void-rgb \
+--model /home/mikkel/final_models/void_rgb/1589053397-n12104-e20-bs4-lr0.0001-void-nyu/model.h5
+
+python evaluate.py \
+--dataset void \
+--channels 3 \
+--use-cpu \
+--use-median-scaling \
+--save \
+--model-name void-rgb-global-gt-scaling \
+--model /home/mikkel/final_models/void_rgb/1589053397-n12104-e20-bs4-lr0.0001-void-nyu/model.h5
+
+python evaluate.py \
+--dataset void \
+--channels 3 \
+--use-cpu \
+--use-median-scaling \
+--use-sparse-depth-scaling \
+--save \
+--model-name void-rgb-global-iD-scaling \
+--model /home/mikkel/final_models/void_rgb/1589053397-n12104-e20-bs4-lr0.0001-void-nyu/model.h5
+
+python evaluate.py \
+--dataset void \
+--channels 3 \
+--use-cpu \
+--use-median-scaling \
+--use-sparse-depth-scaling \
+--use-scaling-array \
+--model-name void-rgb-local-iD-scaling \
+--model /home/mikkel/final_models/void_rgb/1589053397-n12104-e20-bs4-lr0.0001-void-nyu/model.h5
+
+#python evaluate.py \
+#--model /home/mikkel/final_models//model.h5 \
+#--dataset nyu \
+#--channels 3 \
+#--use-cpu \
+#--dont-interpolate \
+#--use-void-1500 \
+#--use-median-scaling \
+#--use-sparse-depth-scaling \
+#--use-scaling-array \
+#--path /home/mikkel/data/pico_sommerhus \
+#--gt-divider 1000.0 \
+#--save
